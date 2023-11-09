e3da4d96b08383320640ef506b3e2d7ca573bb03
Naga Vinay <nagavinay095@gmail.com>
2023-11-09 18:20:05 +0530

    new coommitt23

--- int_setup.sh
+++ int_setup.sh
@@ -2,7 +2,7 @@ echo[$(date)]: "START"
 
 echo[$(date)]: "creating env with python 3.8 version"
 
-conda create --prefix ./env python =3.8-y
+conda create --prefix ./env python =3.8 -y
 
 echo[$(date)]: "activating the environment"
 
